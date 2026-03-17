a961a0dd26881dbb572109619e1f95ddfdd24a7f
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-03-17 18:39:49 -0400

    Finished preliminary entity implementation

--- backend/src/domain/entities/rcf.ts
+++ backend/src/domain/entities/rcf.ts
@@ -9,5 +9,55 @@ export class Rcf {
     private _createdAt: Date;
     private _updatedAt: Date;
 
+    constructor(
+        orgId: string,
+        name: string,
+        licensedBeds: number,
+        currentOpenings: number,
+        isActive: boolean,
+        createdAt: Date,
+        updatedAt: Date,
+    ){
+        this._orgId = orgId;
+        this._name = name;
+        this._licensedBeds = licensedBeds;
+        this._currentOpenings = currentOpenings;
+        this._isActive = isActive;
+        this._createdAt = createdAt;
+        this._updatedAt = updatedAt;
+    }
+
+    public get id(): string {return this._id;}
+    public get orgId(): string {return this._orgId;}
+    public get name(): string {return this._name;}
+    public set name(value: string) {this._name = value;}
+    public get licensedBeds(): number {return this._licensedBeds;}
+    public set licensedBeds(value: number) {this._licensedBeds = value;}
+    public get currentOpenings(): number {return this._currentOpenings;}
+    public set currentOpenings(value: number) {this._currentOpenings = value;}
+    public get isActive(): boolean {return this._isActive;}
+    public set isActive(value: boolean) {this._isActive = value;}
+    public get createdAt(): Date {return this._createdAt;}
+    public set createdAt(value: Date) {this._createdAt = value;}
+    public get updatedAt(): Date {return this._updatedAt;}
+    public set updatedAt(value: Date) {this._updatedAt = value;}
+
+    rcfFactory(
+        orgId: string,
+        name: string,
+        licensedBeds: number,
+        currentOpenings: number,
+        isActive: boolean,
+        createdAt: Date,
+        updatedAt: Date,
+    ){
+        this._orgId = orgId;
+        this._name = name;
+        this._licensedBeds = licensedBeds;
+        this._currentOpenings = currentOpenings;
+        this._isActive = isActive;
+        this._createdAt = createdAt;
+        this._updatedAt = updatedAt;
+    }
 
 }
--- backend/src/domain/entities/rcf_forms.ts
+++ backend/src/domain/entities/rcf_forms.ts
@@ -1,4 +1,4 @@
-export class RcfForms {
+export class RcfForm {
     private _id: string;
     private _rcfId: string;
     private _ownerId: string;
@@ -7,6 +7,58 @@ export class RcfForms {
     private _formType: TemplateType;
     private _contentType: ContentType;
     private _storageKey: string;
+
+    constructor(
+        rcfId: string,
+        ownerId: string,
+        fileName: string,
+        title: string,
+        formType: TemplateType,
+        contentType: ContentType,
+        storageKey: string,
+    ) {
+        this._id = rcfId;
+        this._ownerId = ownerId;
+        this._fileName = fileName;
+        this._title = title;
+        this._formType = formType;
+        this._contentType = contentType;
+        this._storageKey = storageKey;
+    }
+
+    public get id(): string {return this._id;}
+    public get rcfId(): string {return this._rcfId;}
+    public get ownerId(): string {return this._ownerId;}
+    public get fileName(): string {return this._fileName;}
+    public set fileName(value: string) {this._fileName = value;}
+    public get title(): string {return this._title;}
+    public set title(value: string) {this._title = value;}
+    public get formType(): TemplateType {return this._formType;}
+    public set formType(value: TemplateType) {this._formType = value;}
+    public get contentType(): ContentType {return this._contentType;}
+    public set contentType(value: ContentType) {this._contentType = value;}
+    public get storageKey(): string {return this._storageKey;}
+    public set storageKey(value: string) {this._storageKey = value;}
+
+    rcfFormFactory(
+        rcfId: string,
+        ownerId: string,
+        fileName: string,
+        title: string,
+        formType: TemplateType,
+        contentType: ContentType,
+        storageKey: string,
+    ){
+        const rcfForm = new RcfForm(
+            rcfId,
+            ownerId,
+            fileName,
+            title,
+            formType,
+            contentType,
+            storageKey,
+        )
+    }
 
 }
 
