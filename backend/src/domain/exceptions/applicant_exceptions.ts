c97f75512218f40fcad6ee8580796ad497980edf
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-03-27 20:21:11 -0400

    finished implementing preliminary entity rules

---
+++ backend/src/domain/exceptions/applicant_exceptions.ts
@@ -0,0 +1,19 @@
+import { DomainError } from "./domain_error.ts";
+
+export class NoRpIdException extends DomainError {
+    constructor(message: string, error: Error) {
+        super(message, error);
+    }
+}
+
+export class NoApplicationIdException extends DomainError {
+    constructor(message: string, error: Error) {
+        super(message, error);
+    }
+}
+
+export class NoNameException extends DomainError {
+    constructor(message: string, error: Error) {
+        super(message, error);
+    }
+}
